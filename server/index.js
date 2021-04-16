7b2e10f6003281b665e92bc0cfee1671218885b3
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-04-16 14:43:23 +0200

    Serve static content from build package

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-app.use("/dist", express.static(path.join(__dirname, '../src')) )
+app.use("/dist", express.static(path.join(__dirname, '/build', '../src')) )
 
 app.get(["/", "/*"], (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"))
