dc74119b25e74a504097298d8d6b377d19783aaa
Bas Moret <bas.moret@student.hu.nl>
2021-05-18 15:06:51 +0200

    modify person filled at load

--- src/js/component/page/modify-employee-screen.js
+++ src/js/component/page/modify-employee-screen.js
@@ -30,6 +30,9 @@ class ModifyEmployeeScreen extends LitElement {
     static get properties() {
       return {
         results: {type: Array, attribute: false, reflect: true},
+        person: {type: Object, attribute: false, reflect: true},
+        supervisor: {type: String, attribute: false, reflect: true},
+        managers: {type: Object, attribute: false, refelct: true}
       }
     }
 
@@ -41,6 +44,33 @@ class ModifyEmployeeScreen extends LitElement {
     connectedCallback() {
       super.connectedCallback()
       document.addEventListener('provideResults', this._provideResults);
+      this._load()
+    }
+
+    _load = () => {
+        request('GET', `/person/managers`)
+            .then(r => {
+                this.managers = r
+                this.managers.forEach(manager => {
+                    this.results.push({ value: manager.id, name: manager.firstname+" "+manager.lastname })
+                })
+            })
+            .then(_ => {
+                this.loading = false
+                request('GET', `/person/${this.location.params.id}`)
+                    .then(r => {
+                        this.person = r
+                    })
+                    .then(_ => this.loading = false)
+                    .catch(_ => {
+                        this.loading = true;
+                    })
+            })
+            .catch(_ => {
+                this.loading = true;
+            })
+
+
     }
 
     _handleCancel = () => {
@@ -72,27 +102,23 @@ class ModifyEmployeeScreen extends LitElement {
         return  html`
         <cim-top-bar></cim-top-bar>
         <centered-layout>
-          <h1>Medewerker Aanpassen : [naam medewerker]</h1>
+          <h1>Medewerker Aanpassen : ${this.person.firstname+" "+this.person.lastname}</h1>
             <form>
                 <page-segment .title="${"Persoonsgegevens"}" >
-                    <form-item .name="${"firstname"}" .label="${"Voornaam"}">Voornaam</form-item>
-                    <form-item .name="${"lastname"}" .label="${"Achternaam"}">Achternaam</form-item>
-                    <form-item .name="${"email"}" .label="${"Email"}">E-mail</form-item>
+                    <form-item .name="${"firstname"}" .label="${"Voornaam"}" .value="${this.person.firstname}">Voornaam</form-item>
+                    <form-item .name="${"lastname"}" .label="${"Achternaam"}" .value="${this.person.lastname}">Achternaam</form-item>
+                    <form-item .name="${"email"}" .label="${"Email"}" .value="${this.person.email}">E-mail</form-item>
                 </page-segment>
                 <page-segment .title="${"Werkgegevens"}" >
-                    <form-item .name="${"expertise"}" .label="${"Expertise"}">Expertise</form-item>
-                    <form-date-picker .name="${"employedSince"}" .label="${"Werkzaam sinds"}">Werkzaam sinds</form-date-picker>
+                    <form-item .name="${"expertise"}" .label="${"Expertise"}" .value="${this.person.expertise}">Expertise</form-item>
+                    <form-date-picker .name="${"employedSince"}" .label="${"Werkzaam sinds"}" .value="${this.person.employedSince}">Werkzaam sinds</form-date-picker>
                     <form-dropdown-item .name="${"branch"}" .label="${"Filiaal"}" .items="${branchTypes}"
-                    >Branch</form-dropdown-item>
+                        .selected="${this.person.branch}">Branch</form-dropdown-item>
                     <form-dropdown-item .name="${"role"}" .label="${"Rol"}"
-                                        .items="${ roleTypes   
-                                        }"
+                                        .items="${ roleTypes}"
+                                        .selected="${this.person.role}"
                     >Rol</form-dropdown-item>
-                    ${this.results.length > 0 ? html`
-                      <form-dropdown-item .items="${this.results}" .name="${"supervisorId"}" .label="${"Supervisor"}"></form-dropdown-item>
-                    ` : html `
-                      <form-dropdown-item .name="${"supervisorId"}" .label="${"Supervisor"}"></form-dropdown-item>           
-                    `} 
+                      <form-dropdown-item .items="${this.results}" .selected="${this.person.supervisor.personId || ""}" .name="${"supervisorId"}" .label="${"Supervisor"}"></form-dropdown-item>
                     <search-employee .title="${"Supervisor zoeken"}"></search-employee>
                 </page-segment>
             </form>
