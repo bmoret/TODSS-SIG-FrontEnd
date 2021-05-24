cba87c4ff73c4aef4e9a2edaa91fcc5989cba584
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-05-24 11:00:27 +0200

    Remove segment from search-employee Improve search employee page

--- src/js/component/page/search-employee-page.js
+++ src/js/component/page/search-employee-page.js
@@ -1,9 +1,11 @@
 import {css, html, LitElement} from 'lit-element';
 import {store} from "../../state/store/store";
+import {actions} from "../../state/reducer/searchEmployee";
 
 class SearchEmployeePage extends LitElement {
   static get styles() {
     return css`
+    main{display: block;}
       ul {
         list-style: none;
         margin: 0;
@@ -23,6 +25,7 @@ class SearchEmployeePage extends LitElement {
     document.title = "Medewerkers zoeken";
     this.results = [];
     this._provideResults();
+    store.subscribe(this._refresh)
   }
 
   connectedCallback() {
@@ -36,13 +39,19 @@ class SearchEmployeePage extends LitElement {
     this.results = state.segments.results;
   }
 
+  _refresh = async () => {
+    history.replaceState(store.getState(), document.title, window.location)
+    await this.requestUpdate();
+  }
+
   render() {
     return html`
       <app-root>
         <cim-top-bar slot="header"></cim-top-bar>
         <centered-layout slot="body">
           <main>
-          <search-employee .title="${"Medewerkers zoeken"}"></search-employee>
+            <h1>Medewerker zoeken</h1>
+            <search-employee .title="${"Medewerkers zoeken"}"></search-employee>
           </main>
         </centered-layout>
       </app-root>
--- src/js/component/search/search-employee.js
+++ src/js/component/search/search-employee.js
@@ -5,29 +5,13 @@ import { actions } from "../../state/reducer/searchEmployee";
 
 class SearchEmployee extends LitElement {
   static get properties() {
-  return {
-    title: {type: String, attribute: false, reflect: true},
-    results: {type: Array, attribute: false, reflect: true},
+    return {
+      results: {type: Array, attribute: false, reflect: true},
+    }
   }
-}
   constructor() {
     super();
-    this.title = "";
     this.results = [];
-    store.subscribe(this._refresh)
-  }
-
-  _refresh = async () => {
-    history.replaceState(store.getState(), document.title, window.location)
-    await this.requestUpdate();
-  }
-
-  async _handleSegmentToggle(title, isOpen) {
-    if (isOpen) {
-      store.dispatch(actions.close({title: title}))
-    } else {
-      store.dispatch(actions.open({title: title}))
-    }
   }
 
   _search = (e) => {
@@ -41,16 +25,10 @@ class SearchEmployee extends LitElement {
 
   render() {
     const state = store.getState().searchEmployee;
-    const segments = state.segments;
 
     return html`
-      <page-segment 
-      .title="${this.title}"
-      .show="${segments.zoekMedewerker.open}" 
-      @toggle="${_ => this._handleSegmentToggle("zoekMedewerker", segments.zoekMedewerker.open)}">
         <search-bar .placeholder="${"Medewerker naam..."}" @search="${this._search}"></search-bar>
-        <search-person-results .results="${state.results}"></search-person-results>  
-      </page-segment>
+        <search-person-results .results="${state.results}"></search-person-results> 
     `
   }
 }
