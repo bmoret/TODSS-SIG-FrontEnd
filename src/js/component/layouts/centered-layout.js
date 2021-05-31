5f7e04ce63b4fb2df11d503dc51d5ad4003ca6c0
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-05-31 14:20:30 +0200

    Fix centered layout page height

--- src/js/component/layouts/centered-layout.js
+++ src/js/component/layouts/centered-layout.js
@@ -3,13 +3,21 @@ import { LitElement, html, css } from 'lit-element';
 class CenteredLayout extends LitElement {
   static get styles() {
     return css`
+      :host {
+        display: block;
+        position: relative;
+      }
+      
       div {
-        display: inline-block;
+        position: relative;
+        display: block;
         padding: 20px 15px 0 15px;
         margin: auto;
+        min-height: calc(100vh - var(--cim-top-bar-height));
         min-width: 250px;
         max-width: 1350px;
         width: 100%;
+        overflow: visible;
         box-sizing: border-box;
         -webkit-box-shadow: var(--cim-shadow-background);
            -moz-box-shadow: var(--cim-shadow-background); 
