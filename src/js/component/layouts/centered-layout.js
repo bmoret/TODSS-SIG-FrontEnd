fbb267ccdfd57173951c503fd4a77c0d2ea56ec8
ChromaChroma <58219576+ChromaChroma@users.noreply.github.com>
2021-05-31 12:42:09 +0200

    Fix page length

--- src/js/component/layouts/centered-layout.js
+++ src/js/component/layouts/centered-layout.js
@@ -3,18 +3,19 @@ import { LitElement, html, css } from 'lit-element';
 class CenteredLayout extends LitElement {
   static get styles() {
     return css`
+    :host{
+    }
       div {
         position: relative;
-        padding: 0;
-        margin: auto;
-        box-sizing: border-box;
-        width: 100%;
-        height: 100%;
-        min-height: 100vh;
-        max-width: 1350px;
-        min-width: 250px;
         padding: 20px 15px 0 15px;
-        
+        margin: auto;
+        min-height: min-content;
+        height: 100%;
+        min-width: 250px;
+        max-width: 1350px;
+        width: 100%;
+        box-sizing: border-box;
+        overflow: auto;
         -webkit-box-shadow: var(--cim-shadow-background);
            -moz-box-shadow: var(--cim-shadow-background); 
                 box-shadow: var(--cim-shadow-background);
